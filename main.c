5e09824dfeeddc0cc13c314a59e6319132fe1244
Nyveruus <106652476+Nyveruus@users.noreply.github.com>
2026-09-20 19:35:22 +0200

    Main skeleton

---
+++ main.c
@@ -0,0 +1,24 @@
+/* * Each insruction corresponds to 16 symbolic bits (char representation), * open each file, initialize opcode table, initialize symbol table, add predefined symbols to symbol table, * read line by line one buffer for all, for each line (first pass) check for labels and add to symbol table according to instruction number of next,
+ * restart reading, for each line, a or c command?, parse into fields, if a command, notes symbol into symbol table, translate, if symbol found, translate directly,
+ * if c command ,translate fields and append, write line
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+   if (argc !=  3) {
+       fprintf(stderr, "Usage: hack_assembler [infile] [outfile]\n");
+       return 1;
+   }
+   FILE *infile = fopen(argv[1], 'r');
+   if (!infile) {
+      fprintf(stderr, "Error: invalid path\n");
+      return 1;
+   }
+
+   //init tables
+   fields *codetable = NULL;
+   symbol *symboltable = NULL;
+   init_tables(codetable, symboltable);
+}
+
+
